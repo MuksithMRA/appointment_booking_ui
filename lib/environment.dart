88e5a0745d3b71534236ea51681e1bbc8b2466ef
MuksithMRA <muksithmra@gmail.com>
2024-10-08 15:51:05 +0530

    REF

--- lib/environment.dart
+++ lib/environment.dart
@@ -1,4 +1,4 @@
 class Environment {
-  static String apiUrl = 'https://localhost:44350/api';
-  //static String apiUrl = 'https://asirihospitals.azurewebsites.net/api';
+  //static String apiUrl = 'https://localhost:44350/api';
+  static String apiUrl = 'https://asirihospitals.azurewebsites.net/api';
 }
